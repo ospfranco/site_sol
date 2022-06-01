31ff3e42384a038663d039dca0529b79cb7a6a90
Oscar Franco <ospfranco@protonmail.com>
2022-06-01 19:15:09 +0200

    remove unnecesary config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,33 +4,6 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      animation: {
-        blob: 'blob 30s infinite',
-        ring: 'ring 60s infinite',
-      },
-      keyframes: {
-        blob: {
-          from: {
-            transform: 'rotate(0deg)',
-          },
-          to: {
-            transform: 'rotate(360deg)',
-          },
-        },
-        ring: {
-          '0%': {
-            transform: 'translate(-30px, 0px)',
-          },
-          '50%': {
-            transform: 'translate(30px, 0px)',
-          },
-          '100%': {
-            transform: 'translate(-30px, 0px)',
-          },
-        },
-      },
-    },
   },
   plugins: [],
 }
