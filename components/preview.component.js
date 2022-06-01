750f11753e1e6aed5d506c350f2c57471d42c7f1
Oscar Franco <ospfranco@protonmail.com>
2022-06-01 19:00:05 +0200

    Polishing

--- components/preview.component.js
+++ components/preview.component.js
@@ -1,74 +1,87 @@
+import Image from 'next/image'
+import slack from '../public/slack.png'
+import xcode from '../public/xcode.png'
+import iterm from '../public/iterm.png'
+import spotify from '../public/spotify.png'
+import calendar from '../public/calendar.png'
+import code from '../public/code.png'
+import figma from '../public/figma.png'
+import discord from '../public/discord.png'
+
 export const Preview = () => {
   return (
-    <div className="border border-neutral-800 rounded-lg bg-neutral-900 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-60">
+    <div className="border border-neutral-800 rounded-lg bg-neutral-900 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-60 shadow-2xl shadow-gray-800">
       <div className="py-3 px-5">
         <p className="text-neutral-600">Type something...</p>
-        <div className="bg-blue-500 bg-opacity-60 flex rounded-md py-1 px-3 mt-2">
-          <div className="w-4 h-4" />
-          <p>Slack</p>
+        <div className='flex flex-col gap-1 pt-2'>
+          <div className="bg-blue-500 bg-opacity-60 flex rounded-md py-1 px-3 mt-2 gap-2">
+            <Image src={slack} height={22} width={22} alt="slack"/>
+            <p className='text-sm'>Slack</p>
+          </div>
+          <div className="flex py-1 px-3 gap-2">
+            <Image src={xcode} height={22} width={22} alt="slack"/>
+            <p className='text-sm'>Xcode</p>
+          </div>
+          <div className="flex py-1 px-3 gap-2">
+            <Image src={iterm} height={22} width={22} alt="slack"/>
+            <p className='text-sm'>iTerm</p>
+          </div>
+          <div className="flex py-1 px-3 gap-2">
+            <Image src={spotify} height={22} width={22} alt="slack"/>
+            <p className='text-sm'>Spotify</p>
+          </div>
+          <div className="flex py-1 px-3 gap-2">
+            <Image src={calendar} height={22} width={22} alt="slack"/>
+            <p className='text-sm'>Calendar</p>
+          </div>
+          <div className="flex py-1 px-3 gap-2">
+            <Image src={discord} height={22} width={22} alt="slack"/>
+            <p className='text-sm'>Discord</p>
+          </div>
+          <div className="flex py-1 px-3 gap-2">
+            <Image src={code} height={22} width={22} alt="slack"/>
+            <p className='text-sm'>Visual Studio Code</p>
+          </div>
+          <div className="flex py-1 px-3 gap-2">
+            <Image src={figma} height={22} width={22} alt="slack"/>
+            <p className='text-sm'>Figma</p>
+          </div>
         </div>
-        <div className="flex py-1 px-3">
-          <div className="w-4 h-4" />
-          <p>Xcode</p>
-        </div>
-        <div className="flex py-1 px-3">
-          <div className="w-4 h-4" />
-          <p>iTerm</p>
-        </div>
-        <div className="flex py-1 px-3">
-          <div className="w-4 h-4" />
-          <p>iTerm</p>
-        </div>
-        <div className="flex py-1 px-3">
-          <div className="w-4 h-4" />
-          <p>iTerm</p>
-        </div>
-        <div className="flex py-1 px-3">
-          <div className="w-4 h-4" />
-          <p>iTerm</p>
-        </div>
-        <div className="flex py-1 px-3">
-          <div className="w-4 h-4" />
-          <p>iTerm</p>
-        </div>
-        <div className="flex py-1 px-3">
-          <div className="w-4 h-4" />
-          <p>iTerm</p>
-        </div>
+
       </div>
       <div className="border-t border-neutral-800 bg-neutral-800 bg-opacity-80 py-3 px-5">
         <div className="flex border-b border-neutral-700 items-center pb-2">
-          <p className="text-sm">
+          <p className="text-xs">
             Are you going to be my girl{" "}
-            <span className="text-sm text-gray-400">Jet</span>
+            <span className="text-xs text-gray-400">Jet</span>
           </p>
           <div className="mx-3 h-4 border-r border-neutral-700" />
-          <p className="text-sm">
-            28 <span className="text-sm text-gray-400">Sunny</span>
+          <p className="text-xs">
+            28 <span className="text-xs text-gray-400">Sunny</span>
           </p>
         </div>
         <div className="pt-2">
-          <p className="text-sm text-gray-400">Today</p>
+          <p className="text-xs text-gray-400">Today</p>
           <div className="flex py-2 px-3 gap-2 items-center">
             <div className="rounded-full border-2 border-red-500 w-3 h-3" />
-            <p className="text-sm flex-1">Important meeting</p>
-            <p className="text-sm">
+            <p className="text-xs flex-1">Important meeting</p>
+            <p className="text-xs">
               14:00 - <span className="text-gray-400">15:00</span>
             </p>
           </div>
           <div className="flex py-2 px-3 gap-2 items-center">
             <div className="rounded-full border-2 border-blue-500 w-3 h-3" />
-            <p className="text-sm flex-1">Call supplier</p>
-            <p className="text-sm">
+            <p className="text-xs flex-1">Call supplier</p>
+            <p className="text-xs">
               14:00 - <span className="text-gray-400">15:00</span>
             </p>
           </div>
-          <p className="text-sm text-gray-400">Tomorrow</p>
+          <p className="text-xs text-gray-400">Tomorrow</p>
           <div className="flex py-2 px-3 gap-2 items-center">
             <div className="rounded-full border-2 border-green-500 w-3 h-3" />
-            <p className="text-sm flex-1">Important meeting</p>
-            <p className="text-sm">
-              14:00 - <span className="text-gray-400">15:00</span>
+            <p className="text-xs flex-1">Holiday planned</p>
+            <p className="text-xs">
+              All day
             </p>
           </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,11 @@ import Scratchpad from "../public/Scratchpad.png";
 
 export default function Home() {
   return (
-    <div className="overflow-y-scroll">
-      <div className="py-3 mx-auto w-full max-w-3xl flex">
+    <div >
+      <div className="py-3 px-6 mx-auto w-full max-w-3xl flex sticky top-0 bg-black bg-opacity-60 bg-clip-padding backdrop-filter backdrop-blur-sm z-10">
         <Image src={Logo} height={29} width={78} alt="Sol logo" />
         <div className="flex-1" />
-        <a className="text-black bg-white px-3 rounded text-sm flex items-center justify-center">
+        <a className="text-black bg-white px-3 rounded text-sm flex items-center justify-center hover:cursor-pointer hover:text-white border border-white hover:bg-black transition" href="https://github.com/ospfranco/sol/tree/main/releases">
           Download
         </a>
       </div>
@@ -39,7 +39,7 @@ export default function Home() {
           tweaking)
         </h4>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 py-10">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 py-10">
           <div className="rounded-lg bg-neutral-800 overflow-hidden">
             <div className="h-32 relative">
               <Image
@@ -51,9 +51,11 @@ export default function Home() {
                 objectFit="cover"
               />
             </div>
-            <div className="p-3">
-              <h5 className="font-bold">Google Maps</h5>
-              <p className="text-gray-400">Quickly lookup adresses</p>
+            <div className="px-6 py-5">
+              <h5 className="">Google Maps</h5>
+              <p className="mt-1 text-sm text-gray-400">
+                Quickly lookup adresses
+              </p>
             </div>
           </div>
           <div className="rounded-lg bg-neutral-800 overflow-hidden">
@@ -67,9 +69,11 @@ export default function Home() {
                 objectFit="cover"
               />
             </div>
-            <div className="p-3">
-              <h5 className="font-bold">Emoji Picker</h5>
-              <p className="text-gray-400">Faster and more reliable</p>
+            <div className="px-6 py-5">
+              <h5 className="">Emoji Picker</h5>
+              <p className="mt-1 text-sm text-gray-400">
+                Faster and more reliable
+              </p>
             </div>
           </div>
           <div className="rounded-lg bg-neutral-800 overflow-hidden">
@@ -83,9 +87,11 @@ export default function Home() {
                 objectFit="cover"
               />
             </div>
-            <div className="p-3">
-              <h5 className="font-bold">Gif Picker</h5>
-              <p className="text-gray-400">Find that perfect Gif</p>
+            <div className="px-6 py-5">
+              <h5 className="">Gif Picker</h5>
+              <p className="mt-1 text-sm text-gray-400">
+                Find that perfect Gif
+              </p>
             </div>
           </div>
           <div className="rounded-lg bg-neutral-800 overflow-hidden">
@@ -99,9 +105,11 @@ export default function Home() {
                 objectFit="cover"
               />
             </div>
-            <div className="p-3">
-              <h5 className="font-bold">Clipboard Manager</h5>
-              <p className="text-gray-400">Never loose a copied text</p>
+            <div className="px-6 py-5">
+              <h5 className="">Clipboard Manager</h5>
+              <p className="mt-1 text-sm text-gray-400">
+                Never loose a copied text
+              </p>
             </div>
           </div>
           <div className="rounded-lg bg-neutral-800 overflow-hidden">
@@ -115,9 +123,11 @@ export default function Home() {
                 objectFit="cover"
               />
             </div>
-            <div className="p-3">
-              <h5 className="font-bold">Window Manager</h5>
-              <p className="text-gray-400">Quickly organize your desktop</p>
+            <div className="px-6 py-5">
+              <h5 className="">Window Manager</h5>
+              <p className="mt-1 text-sm text-gray-400">
+                Quickly organize your desktop
+              </p>
             </div>
           </div>
           <div className="rounded-lg bg-neutral-800 overflow-hidden">
@@ -131,26 +141,31 @@ export default function Home() {
                 objectFit="cover"
               />
             </div>
-            <div className="p-3">
-              <h5 className="font-bold">Note Scratchpad</h5>
-              <p className="text-gray-400">Take a note from anywhere</p>
+            <div className="px-6 py-5">
+              <h5 className="">Note Scratchpad</h5>
+              <p className="mt-1 text-sm text-gray-400">
+                Take a note from anywhere
+              </p>
             </div>
           </div>
         </div>
       </div>
 
       <h1 className="text-center font-bold text-5xl mt-32">Get it now</h1>
-      <h4 className="text-lg text-gray-300 text-center max-w-lg mx-auto pt-3">
-        Download it from source or build it yourself
-      </h4>
 
-      <a className="text-black bg-white px-3 rounded flex items-center justify-center w-44 mx-auto py-3 mt-12">
+      <a className="text-black bg-white px-3 rounded flex items-center justify-center w-44 mx-auto py-3 mt-12 hover:cursor-pointer hover:text-white border border-white hover:bg-black transition" href="https://github.com/ospfranco/sol/tree/main/releases">
         Download
+      </a>
+      <h4 className="text-gray-300 text-center max-w-lg mx-auto mt-10">
+        or check out the source code
+      </h4>
+      <a className="border px-3 rounded flex items-center justify-center w-44 mx-auto py-3 mt-6 border-neutral-400 hover:border-white transition cursor-pointer" href="https://github.com/ospfranco/sol">
+        Source
       </a>
 
       <div className="bg-neutral-900 p-6 mt-32">
         <div className="py-3 mx-auto w-full max-w-3xl">
-          <p className="text-center text-neutral-200">Built by Oscar Franco</p>
+          <p className="text-center text-neutral-200 text-sm">Built by <span className="font-bold">Oscar Franco</span></p>
         </div>
       </div>
     </div>
