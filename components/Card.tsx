92e422d13e76cf23d239a5ccf7ef8bdc8910ccbd
Sandipan <sandipan3002@gmail.com>
2026-01-30 12:54:13 +0530

    fix: added min height in card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,7 +6,7 @@ type CardProps = {
 
 export const Card = ({title, text, BottomComponent}: CardProps) => {
   return (
-    <div className="border rounded-3xl p-8 border-neutral-800 sm:flex-1 gap-2 flex flex-col h-48 items-center bg-neutral-900 bg-opacity-50">
+    <div className="border rounded-3xl p-8 border-neutral-800 sm:flex-1 gap-2 flex flex-col min-h-[12rem] items-center bg-neutral-900 bg-opacity-50">
       <p className="text-xl font-semibold">{title}</p>
       <p className="text-gray-300">{text}</p>
       <div className="flex-1" />
